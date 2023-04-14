a13cb55e4127bda2f0926cb1461b7acae54554d8
eddyizm <eddyizm@gmail.com>
2023-04-13 21:07:05 -0700

    renamed pydantic model, fixed format and es id reference, cleaned up unused code

--- crud.py
+++ crud.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session #we want session so we can declare the type of db parameters
-import models, schemas
+from models import PatientData
 
 def get_patient_by_esid(db: Session, es_id: int):
     print("Hey!")
-    print(db.query(models.PatientData).filter(models.PatientData.es_id == es_id))
+    print(db.query(PatientData).filter(PatientData.es_id == es_id))
     print("Aye")
-    print(db.query(models.PatientData))
+    print(db.query(PatientData))
     print("Crey!")
-    return db.query(models.PatientData).filter(models.PatientData.es_id == es_id).first()
+    return db.query(PatientData).filter(PatientData.es_id == es_id).first()
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud, models, schemas, database
+import crud, database
+from schemas import Patient
 # from .database import SessionLocal, engine
-
-models.database.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
-
 # Dependency
 def get_db(): # gets called to start the communcication with db
+    # I'd probably move this to the database file ~eddyizm~
     db = database.SessionLocal()
     try:
         yield db
@@ -20,8 +17,7 @@ def get_db(): # gets called to start the communcication with db
         db.close()
 
 
-# @app.get("/sensor/Tables/bed_data_3054_only/{es_id}", response_model=schemas.PatientDataBase)
-@app.get("/Schemas/sensor/Tables/bed_data_3054_only/{es_id}", response_model=schemas.PatientDataBase)
+@app.get("/Schemas/sensor/Tables/bed_data_3054_only/{es_id}", response_model=Patient)
 def read_patient_by_esid(es_id: int, db: Session = Depends(get_db)):
     db_patient = crud.get_patient_by_esid(db, es_id=es_id)
     if db_patient is None:
