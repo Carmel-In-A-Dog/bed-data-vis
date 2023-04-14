975f1fbb3e1106a94c12b81102e2c0f9acf08a3b
Carmel-In-A-Dog <98280809+Carmel-In-A-Dog@users.noreply.github.com>
2023-04-13 19:29:08 -0500

    Removed comments

--- schemas.py
+++ schemas.py
@@ -15,33 +15,3 @@ class PatientDataBase(BaseModel):
 
 class PatientData(PatientDataBase):
     pass
-
-# class ItemBase(BaseModel):
-#     title: str
-#     description: Union[str, None] = None # might be busted
-    
-# class ItemCreate(ItemBase):
-#     pass
-
-# class Item(ItemBase):
-#     id: int
-#     owner_id: int
-    
-#     # use this so we get relationship data with sqlalchemy
-#     class Config:
-#         orm_mode = True
-        
-# class UserBase(BaseModel):
-#     email: str
-
-# class UserCreate(UserBase):
-#     password: str
-
-# class User(UserBase):
-#     id: int
-#     is_active: bool
-#     items: List[Item] = []
-    
-#     class Config:
-#         orm_mode = True
-    
