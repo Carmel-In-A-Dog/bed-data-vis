d232f06369d635c606a320633b4a1898611fa8d3
Carmel-In-A-Dog <98280809+Carmel-In-A-Dog@users.noreply.github.com>
2023-04-13 19:28:43 -0500

    Removed comments

--- models.py
+++ models.py
@@ -10,30 +10,3 @@ class PatientData(database.Base):
     tstamp_base = Column(String, index=True)
     ts_offset_seconds = Column(String) #should be float
     data = Column(String, index=True)
-    
-    
-
-
-#this class is a model b/c it interacts with the db
-# class User(Base):
-#     __tablename__ = "users"
-    
-#     id = Column(Integer, primary_key=True, index=True)
-#     email = Column(String, unique=True, index=True)
-#     hashed_password = Column(String)
-#     is_active = Column(Boolean, default=True)
-    
-#     # contains values from other tables related to this one
-#     items = relationship("Item", back_populates="owner")
-    
-# class Item(Base):
-#     __tablename__ = "items"
-    
-#     id = Column(Integer, primary_key=True, index=True)
-#     title = Column(String, index=True)
-#     description = Column(String, index=True)
-#     owner_id = Column(Integer, ForeignKey("users.id"))
-    
-#     owner = relationship("User", back_populates="items")
-    
-    
